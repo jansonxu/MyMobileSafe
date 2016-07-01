e2f9b86f530cb7b416f6e00bbcd2c2bf676884a3
jansonxu <jansonxu@sina.cn>
2016-07-01 10:25:02 +0800

    实验github好用否？

--- app/src/main/java/com/l000phone/mymobilesafe/MainActivity.java
+++ app/src/main/java/com/l000phone/mymobilesafe/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //项目经理你好，俺入伙了哦！呵呵。。。。
     }
 }
